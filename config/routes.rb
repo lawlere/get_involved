9ca6f6106be14f386c3849a807da701324d95364
Suny Gao <suny.gao@evb.com>
2013-04-09 11:42:52 -0700

    updating mark up of top nav, making all routes lowercase, updating name of cause route from marriel to cause

--- config/routes.rb
+++ config/routes.rb
@@ -7,12 +7,12 @@ RailsPrelaunchSignup::Application.routes.draw do
   resources :users
 
   match "/home" => "home#index"
-  match "/About" => "home#about"
-  match "/What" => "home#what"
-  match "/Story" => "home#story"
-  match "/Marriel" => "people#marriel"
+  match "/about" => "home#about"
+  match "/what" => "home#what"
+  match "/story" => "home#story"
+  match "/cause" => "people#marriel"
   match "/signup" => "signup#signup"
   match "/blog" => "home#blog"
-  match "/Donate" => redirect("http://www.indiegogo.com/projects/kubmo"), :as => :Donate 
+  match "/donate" => redirect("http://www.indiegogo.com/projects/kubmo"), :as => :Donate 
 
 end
